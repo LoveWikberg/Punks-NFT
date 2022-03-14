8d4524d7ab37635a2640d8ad1972170fb3819210
Love <love.wikberg@soprasteria.com>
2022-03-14 15:25:09 +0100

    I understand now

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import MintPunkContract from "./contracts/MintPunks.json";
 import getWeb3 from "./getWeb3";
 import "./App.css";
+import { ethers } from "ethers";
 
 function App() {
   const [storageValue, setStorageValue] = useState(0);
   const [web3, setWeb3] = useState(null);
   const [accounts, setAccounts] = useState(null);
-  const [contract, setContract] = useState(null);
   const [mintContract, setMintContract] = useState(null);
   const [ownedTokes, setOwnedTokes] = useState([]);
   const [amountTokes, setAmountTokes] = useState();
+  const [mintPrice, setMintPrice] = useState();
 
   useEffect(() => {
     const initiate = async () => {
@@ -24,34 +24,28 @@ function App() {
   const initStates = async () => {
     try {
       const web3 = await getWeb3();
-
       const accounts = await web3.eth.getAccounts();
       console.log(accounts);
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
       const deployedNetwork2 = MintPunkContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
 
       const instance2 = new web3.eth.Contract(
         MintPunkContract.abi,
         deployedNetwork2 && deployedNetwork2.address,
       );
 
-      const tokens = await instance2.methods.tokensOfOwner().call();
+      const tokens = await instance2.methods.tokensOfOwner().call({ from: accounts[0] });
       console.log(tokens);
 
-      const amountok = await instance2.methods.totalSupp().call();
-      // await instance.methods.set(5).send({ from: accounts[0] });
-      // const response = await instance.methods.get().call();
+      const amountok = await instance2.methods.totalSupp().call({ from: accounts[0] });
+
+      const price = await instance2.methods.TOKEN_PRICE().call();
+      setMintPrice(price);
 
       setOwnedTokes(tokens);
       setAmountTokes(amountok);
       setWeb3(web3);
       setAccounts(accounts);
-      setContract(instance);
       setMintContract(instance2);
       // setStorageValue(response);
 
@@ -63,31 +57,38 @@ function App() {
     }
   }
 
-  // const changeContractValue = async (e) => {
-  //   if (e.keyCode === 13) {
-  //     await contract.methods.set(e.target.value).send({ from: accounts[0] });
-  //       // .then(async () => await updatefan());
-  //   }
-  // };
-
   const updatefan = async () => {
-    const tokenaaa = await mintContract.methods.tokenURIData(1).call();
-    console.log(tokenaaa);
-    const response = await mintContract.methods.getSenderTest().call();
+    const response = await mintContract.methods.getSenderTest().call({ from: accounts[0] });
     console.log(response);
     setStorageValue(response);
   }
 
   const mintTest = async (e) => {
     if (e.keyCode === 13) {
-      const value = parseInt(e.target.value);
-      const price = parseInt(0.01, 10);
-      console.log(price * value);
-      await mintContract.methods.presaleMint(value)
-        .send({ from: accounts[0], value: price * value });
+      const amount = parseInt(e.target.value);
+      // const price = parseInt(mintPrice, 10);
+      console.log(mintPrice * amount);
+      console.log(mintPrice);
+      console.log(amount);
+      console.log(ethers.BigNumber.from(mintPrice));
+
+      await mintContract.methods.presaleMint(amount)
+        .send({ from: accounts[0], value: mintPrice * amount });
     }
-    // const tasd = await mintContract.methods.getSenderTest().call();
-    // console.log(tasd);
+  }
+
+  const sharesfan = async () => {
+    const response = await mintContract.methods.getTotalShares().call();
+    console.log(response);
+  }
+
+  const reciverfan = async () => {
+    await mintContract.methods.releaseShares("0xEAE1b5133f97fbbe0ab8b7578907d395d41ab46b").send({ from: accounts[0] });
+  }
+
+  const recivedfan = async () => {
+    const response = await mintContract.methods.releasedShares().call();
+    console.log(ethers.utils.formatEther(response));
   }
 
   return (
@@ -95,10 +96,14 @@ function App() {
       {
         !web3
           ? <div>Loading Web3, accounts, and contract...</div>
-          : <div className="App">
+          :
+          <div className="App">
             {/* <input placeholder="Skriv ett nummer och klicka enter" onKeyDown={async (e) => await changeContractValue(e)} /> */}
             <input placeholder="Antal punks?" onKeyDown={async (e) => await mintTest(e)} />
             <button onClick={() => updatefan()}>update</button>
+            <button onClick={() => sharesfan()}>get total shares</button>
+            <button onClick={() => reciverfan()}>recieve</button>
+            <button onClick={() => recivedfan()}>total recieved</button>
             <h1>{amountTokes}</h1>
             {
               ownedTokes.map((token, key) =>
--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var MintPunks = artifacts.require("./MintPunks.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(MintPunks);
 };
